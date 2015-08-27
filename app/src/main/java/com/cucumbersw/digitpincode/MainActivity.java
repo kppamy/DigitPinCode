66ce003ef0d5f478b0104d1d3b6413d972644570
huangwei <huangwei.robin@gmail.com>
2015-08-27 10:07:36 +0800

    Initial commit with library and demo app.

---
+++ app/src/main/java/com/cucumbersw/digitpincode/MainActivity.java
@@ -0,0 +1,76 @@
+package com.cucumbersw.digitpincode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.cucumbersw.digitpincodeview.DigitPinCodeView;
+
+public class MainActivity extends AppCompatActivity implements DigitPinCodeView.PinCodeCompleteListener, View.OnClickListener {
+    private DigitPinCodeView mPinCodeView;
+    private Button mBtnConfirm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPinCodeView = (DigitPinCodeView) findViewById(R.id.pincodes);
+        mPinCodeView.setPinCodeCompleteListener(this);
+        mBtnConfirm = (Button)findViewById(R.id.btn_confirm);
+        mBtnConfirm.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPinCodeView.requestFocus();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onInputComplete(int[] pinCodes, int length) {
+        mPinCodeView.clearFocus();
+        mBtnConfirm.requestFocus();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_confirm) {
+            int[] pinCodes = mPinCodeView.getPinCodes();
+            int length = pinCodes.length;
+            String PIN = "";
+            for (int i = 0; i < length; i ++) {
+                if (pinCodes[i] < 0 || pinCodes[i] > 9) {
+                    Toast.makeText(this, String.format("Invalid input, PIN[%d] code is %d", i, pinCodes[i]), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                PIN += String.valueOf(pinCodes[i]);
+            }
+            Toast.makeText(this, String.format("PIN code is %s", PIN), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ digitpincodeview/src/main/java/com/cucumbersw/digitpincodeview/DigitPinCodeView.java
@@ -0,0 +1,155 @@
+package com.cucumbersw.digitpincodeview;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ */
+public class DigitPinCodeView extends LinearLayout{
+    public static final int INVALID_PIN = -1;
+    private static final int NUM_OF_PINS = 4;
+    private TextView[] mPinCodes = new TextView[NUM_OF_PINS];
+    private int[] mPinValues = new int[4];
+    private int mPos = 0; //current input
+
+    public DigitPinCodeView(Context context) {
+        this(context, null);
+    }
+
+    public DigitPinCodeView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DigitPinCodeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        for (int i = 0; i < NUM_OF_PINS; i ++) {
+            mPinValues[i] = INVALID_PIN;
+            mPinCodes[i] = (TextView)inflater.inflate(R.layout.digitpin, null, false);
+            int l = (int)(mPinCodes[i].getTextSize() * 2);
+            this.addView(mPinCodes[i], new ViewGroup.LayoutParams(l, l));
+        }
+    }
+
+    @Override
+    protected void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
+        if (gainFocus) {
+            mPinCodes[mPos].setBackgroundResource(R.drawable.bg_highlight_with_border_dark);
+        } else {
+            mPinCodes[mPos].setBackgroundResource(R.drawable.bg_normal_with_border_dark);
+        }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(@NonNull KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
+                if (mPos > 0 && mPinValues[mPos] == INVALID_PIN) {
+                    //current position has no input yet, directly move backward
+                    mPos --;
+                }
+                mPinValues[mPos] = INVALID_PIN;
+                //high light current position
+                mPinCodes[mPos].setBackgroundResource(R.drawable.bg_highlight_with_border_dark);
+                mPinCodes[mPos].setText(""); //Clear the pin code at current position
+                if (mPos < NUM_OF_PINS - 1) { // clear the high light for former position
+                    mPinCodes[mPos + 1].setBackgroundResource(R.drawable.bg_normal_with_border_dark);
+                }
+                mPos--; //move the position backward
+                if (mPos < 0) { //no backspace when nothing has been input
+                    mPos = 0;
+                }
+            } else {
+                int value;
+                switch (event.getKeyCode()) {
+                    case KeyEvent.KEYCODE_0:
+                    case KeyEvent.KEYCODE_NUMPAD_0:
+                        value = 0;
+                        break;
+                    case KeyEvent.KEYCODE_1:
+                    case KeyEvent.KEYCODE_NUMPAD_1:
+                        value = 1;
+                        break;
+                    case KeyEvent.KEYCODE_2:
+                    case KeyEvent.KEYCODE_NUMPAD_2:
+                        value = 2;
+                        break;
+                    case KeyEvent.KEYCODE_3:
+                    case KeyEvent.KEYCODE_NUMPAD_3:
+                        value = 3;
+                        break;
+                    case KeyEvent.KEYCODE_4:
+                    case KeyEvent.KEYCODE_NUMPAD_4:
+                        value = 4;
+                        break;
+                    case KeyEvent.KEYCODE_5:
+                    case KeyEvent.KEYCODE_NUMPAD_5:
+                        value = 5;
+                        break;
+                    case KeyEvent.KEYCODE_6:
+                    case KeyEvent.KEYCODE_NUMPAD_6:
+                        value = 6;
+                        break;
+                    case KeyEvent.KEYCODE_7:
+                    case KeyEvent.KEYCODE_NUMPAD_7:
+                        value = 7;
+                        break;
+                    case KeyEvent.KEYCODE_8:
+                    case KeyEvent.KEYCODE_NUMPAD_8:
+                        value = 8;
+                        break;
+                    case KeyEvent.KEYCODE_9:
+                    case KeyEvent.KEYCODE_NUMPAD_9:
+                        value = 9;
+                        break;
+                    default:
+                        return false;
+                }
+                mPinValues[mPos] = value;
+//                mPinCodes[mPos].setText(String.format("%d", value));
+                mPinCodes[mPos].setText("●");
+                // clear high light on current position
+                mPinCodes[mPos].setBackgroundResource(R.drawable.bg_normal_with_border_dark);
+                mPos ++; //move pos forward
+                if (mPos > NUM_OF_PINS - 1) { //all pin codes are input
+                    mPos = NUM_OF_PINS - 1; //stay at the last position
+                    onInputComplete();
+                } else { //high light the next position
+                    mPinCodes[mPos].setBackgroundResource(R.drawable.bg_highlight_with_border_dark);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private void onInputComplete() {
+        if (mInputListener != null) {
+            mInputListener.onInputComplete(mPinValues, NUM_OF_PINS);
+        }
+    }
+
+    private PinCodeCompleteListener mInputListener;
+    public void setPinCodeCompleteListener(PinCodeCompleteListener l) {
+        mInputListener = l;
+    }
+
+    public interface PinCodeCompleteListener {
+        void onInputComplete(int[] pinCodes, int length);
+    }
+
+    public int[] getPinCodes() {
+        return mPinValues;
+    }
+}
